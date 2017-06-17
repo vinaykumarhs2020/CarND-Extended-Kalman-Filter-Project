ddc6252728507beb507983932e3ad05ef80a4c33
vinay <me@vnay.in>
2017-06-17 08:14:22 -0700

    rmse

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,16 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  VectorXd _diff(4);
+  for (size_t i = 0; i < estimations.size(); i++) {
+    _diff =  ground_truth[i] - estimations[i]; // Diff
+    _diff = _diff.array().square(); // Square
+    rmse += _diff;
+  }
+  rmse = rmse / estimations.size(); // Mean
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
