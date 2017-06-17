d69489918955019e2b6039d9290cf3e91eceaa48
vinay <me@vnay.in>
2017-06-17 08:20:52 -0700

    Calculate Jacobian

--- src/tools.cpp
+++ src/tools.cpp
@@ -32,4 +32,28 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  // Declare Hj:
+  MatrixXd Hj(3,4);
+  // Seperate the terms in state vector
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  // Calculate intermediate terms
+  float c1 = px*px + py*py;
+  float c2 = sqrt(c1);
+  float c3 = c1 * c2;
+
+  // Check for division by zero:
+  if (c1 < 0.00001){
+    std::cout << "Division by zero!!!" << std::endl;
+    return Hj;
+  }
+
+  Hj << (px/c2), (py/c2), 0, 0,
+        -(py/c1), (px/c1), 0, 0,
+        py*(vx*py-vy*py)/c3, px*(px*vy-vx*py)/c3, px/c2, py/c2;
+
+  return Hj;
 }
